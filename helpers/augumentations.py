f21cec7f706b76736b2937ccf0372b2ccd8e0a29
Sławomir Paszko <folkus@gmail.com>
2023-07-18 14:57:58 +0200

    Transformations fixes.

--- helpers/augumentations.py
+++ helpers/augumentations.py
@@ -9,32 +9,43 @@ from helpers.hashing import GetRandomSha1
 
 # Shape : Albumentations transform
 transform_shape = A.Compose([
-    A.RandomCrop(width=640, height=352, p=0.3),
-    A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2,
-                       rotate_limit=15, p=0.7),
-    A.ElasticTransform(alpha_affine=9, p=0.2),
-    A.OpticalDistortion(p=0.2),
+    A.RandomCrop(width=800, height=600, p=0.3),
+    A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=15,
+                       p=0.7, border_mode=cv2.BORDER_CONSTANT),
+    A.ElasticTransform(alpha_affine=9, p=0.2, border_mode=cv2.BORDER_CONSTANT),
+    A.OpticalDistortion(distort_limit=0.2, p=0.2,
+                        border_mode=cv2.BORDER_CONSTANT),
     A.ZoomBlur(max_factor=1.1, p=0.2),
 ], bbox_params=A.BboxParams(format='yolo', min_area=100, min_visibility=0.2))
 
 # Color : Albumentations transform
 transform_color = A.Compose([
-    A.JpegCompression(quality_lower=20, quality_upper=55, p=0.3),
-    A.RandomBrightnessContrast(p=0.3),
-    A.RandomContrast(p=0.3),
-    A.RandomToneCurve(scale=0.5, p=0.3),
-    A.HueSaturationValue(p=0.3),
-    A.ColorJitter(p=0.2),
-    A.MultiplicativeNoise(p=0.2),
-    A.Downscale(scale_min=0.3, scale_max=0.6, p=0.2),
-    A.MedianBlur(blur_limit=5, p=0.1),
-    A.ISONoise(color_shift=(0.01, 0.08), intensity=(0.2, 0.8), p=0.1),
-    A.PixelDropout(dropout_prob=0.1, p=0.1),
-    A.RandomRain(drop_length=10, blur_value=4, p=0.1),
-    A.Spatter(p=0.1),
-    A.RandomSnow(p=0.1),
-    A.RandomSunFlare(p=0.1),
-    A.RandomFog(p=0.1),
+    # Colors : Brightnes, contrast, tone curve, hue, saturation, value
+    A.SomeOf([
+        A.RandomBrightnessContrast(p=0.3),
+        A.RandomToneCurve(scale=0.5, p=0.3),
+        A.HueSaturationValue(p=0.3),
+        A.ColorJitter(p=0.2),
+    ], n=2),
+    # Quality : Jpeg compression, multiplicative noise, downscale
+    A.OneOf([
+        A.ImageCompression(quality_lower=30, quality_upper=55, p=0.3),
+        A.MultiplicativeNoise(p=0.2),
+        A.Downscale(scale_min=0.4, scale_max=0.6, p=0.2),
+        A.MedianBlur(blur_limit=5, p=0.1),
+        A.ISONoise(color_shift=(0.01, 0.08), intensity=(0.2, 0.8), p=0.1),
+        A.PixelDropout(dropout_prob=0.1, p=0.1),
+        A.Spatter(p=0.1),
+    ]),
+    # Weather : Dropouts, rain, snow, sun flare, fog
+    A.OneOf([
+        A.RandomRain(drop_length=10, blur_value=4, p=0.1),
+        A.RandomSnow(p=0.1),
+        A.RandomSunFlare(src_radius=260, num_flare_circles_lower=2,
+                         num_flare_circles_upper=6, p=0.1),
+        A.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.5,
+                    alpha_coef=0.5, p=0.1),
+    ]),
 ], bbox_params=A.BboxParams(format='yolo', min_area=100, min_visibility=0.2))
 
 # All : Full transform
